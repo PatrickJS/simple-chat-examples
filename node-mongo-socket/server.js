fb60db5f98c49dab04cb6d50ccd6d71a7196399e
gdi2290 <github@gdi2290.com>
2014-02-15 19:53:11 -0800

    log socket connection

--- node-mongo-socket/server.js
+++ node-mongo-socket/server.js
@@ -2,3 +2,7 @@ var mongo = require('mongodb').MongoClient;
 
 var client = require('socket.io').listen(8080).sockets;
 
+
+client.on('connection', function(socket) {
+  console.log('Someone has connected');
+});
