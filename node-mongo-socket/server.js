265c9d5d43eb62cf5ac377e75107370821581865
gdi2290 <github@gdi2290.com>
2014-02-15 20:03:55 -0800

    require bluebird

--- node-mongo-socket/server.js
+++ node-mongo-socket/server.js
@@ -1,7 +1,22 @@
-var mongo = require('mongodb').MongoClient;
+'use strict';
 
-var client = require('socket.io').listen(8080).sockets;
+var mongo   = require('mongodb').MongoClient;
+var Promise = require('bluebird');
+var client  = require('socket.io');
 
+var config = {
+
+  mongo: {
+    url: 'mongodb://127.0.0.1/chat'
+  },
+
+  socket: {
+    port: 8080
+  }
+
+};
+
+client = client.listen(config.socket.port).sockets;
 
 client.on('connection', function(socket) {
   console.log('Someone has connected');
